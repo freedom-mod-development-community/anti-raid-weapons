10a37cb6a93c4d0e1108b3ef2da504e1a39002cc
YukikazeScarlet <scarlet.yukikaze@gmail.com>
2026-08-28 17:53:45 +0900

    erase annotation warn

--- src/main/java/xyz/fmdc/arw/spq9b/Spq9bBlock.java
+++ src/main/java/xyz/fmdc/arw/spq9b/Spq9bBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xyz.fmdc.arw.registry.ModBlocks;
 
@@ -20,20 +21,20 @@ public class Spq9bBlock extends BaseEntityBlock {
     // BlockEntity（データ・回転処理）の生成
     @Nullable
     @Override
-    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+    public BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
         return new Spq9bBlockEntity(pos, state);
     }
 
     // 毎Tickの回転処理（tickメソッド）を呼ぶための設定
     @Nullable
     @Override
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> type) {
         return createTickerHelper(type, ModBlocks.SPQ9B.getBEType(), Spq9bBlockEntity::tick);
     }
 
     // バニラのブロックレンダラー（キューブ描画）を無効化し、BER (Ops39Renderer) のみで描画させる
     @Override
-    public RenderShape getRenderShape(BlockState state) {
+    public @NotNull RenderShape getRenderShape(@NotNull BlockState state) {
         return RenderShape.ENTITYBLOCK_ANIMATED;
     }
 }
