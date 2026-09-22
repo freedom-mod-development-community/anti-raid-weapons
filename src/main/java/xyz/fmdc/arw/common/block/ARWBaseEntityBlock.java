7e05b6664c3e2d700dd3ecf311aa65309f8e8d53
YukikazeScarlet <scarlet.yukikaze@gmail.com>
2026-09-22 18:25:47 +0900

    ARWBaseEntityBlockを継承したEntityBlockのuseにおける非推奨警告の削除.

--- src/main/java/xyz/fmdc/arw/common/block/ARWBaseEntityBlock.java
+++ src/main/java/xyz/fmdc/arw/common/block/ARWBaseEntityBlock.java
@@ -1,12 +1,19 @@
 package xyz.fmdc.arw.common.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class ARWBaseEntityBlock extends BaseEntityBlock {
@@ -28,4 +35,12 @@ public abstract class ARWBaseEntityBlock extends BaseEntityBlock {
         // 設置したプレイヤーに向かい合う方向（または getHorizontalDirection() でプレイヤーの向いている方向）
         return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public @NotNull InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player,
+                                          @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+        return InteractionResult.PASS;
+    }
+
 }
